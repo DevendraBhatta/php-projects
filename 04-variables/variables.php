107ec638d917cd7ad11bb0c1f2aec909bd529dc4
DevendraBhatta <devendrabhatta63@gmail.com>
2026-01-07 12:49:57 +0545

    Creating a Variable

--- 04-variables/variables.php
+++ 04-variables/variables.php
@@ -1,6 +1,13 @@
 <?php
 
-$greeting  = "Hello PHP";
+$greeting  = "Hello PHP! from mars";
+$year = 2050;
+
 echo $greeting;
+echo $greeting;
+echo $greeting;
+echo $greeting;
+echo $greeting;
+echo $year;
 
 ?>
