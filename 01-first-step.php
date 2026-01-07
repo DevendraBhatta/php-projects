3eed754977e3b1b5115b8964a1452dedaf653e2a
DevendraBhatta <devendrabhatta63@gmail.com>
2026-01-07 12:35:23 +0545

    first-commit

---
+++ 01-first-step.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?php echo "Hello from PHP"; ?></h1>
+    <h1>This is my First PHP file</h1>
+    <h1>A Dice Roll:<?php echo rand(1, 6); ?></h1>
+
+</body>
+
+</html>
---
+++ 04-variables/variables.php
@@ -0,0 +1,6 @@
+<?php
+
+$greeting  = "Hello PHP";
+echo $greeting;
+
+?>
